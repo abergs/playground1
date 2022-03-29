ca95a31117d6c7818969e70b3e8903c7df8ae129
Anders Åberg <anders@andersaberg.com>
2022-03-30 01:44:09 +0200

    deploy

---
+++ assets/bb2.9d95b556.js
@@ -0,0 +1,313 @@
+import"./modulepreload-polyfill.b7f2da20.js";import{T as x,G as d,S as y,f as m,b as z,n as p,g as u,P,W as w,O as h,i as _}from"./vendor.23931e5f.js";var b="/playground1/assets/texture.fb0f5fdc.jpg",S="/playground1/assets/r2.dee41ee3.png",U="/playground1/assets/b2.0dc69ad7.png";const M=`
+varying vec2 vUv;
+uniform float uTime;
+uniform vec2 uResolution;
+uniform float scaleUp;
+varying float noise;
+uniform sampler2D alphaMap;
+
+
+float map(float value, float min1, float max1, float min2, float max2) {
+  return min2 + (value - min1) * (max2 - min2) / (max1 - min1);
+}
+
+vec2 skew (vec2 st, float a1, float a2) {
+    vec2 r = vec2(0.0);
+    r.x = a1 * st.x;
+    r.y = st.y + a2 *r.x;
+    return r;
+}
+
+float random (vec2 st) {
+    return fract(sin(dot(st.xy,
+                         vec2(12.9898,78.233)))*
+        43758.5453123);
+}
+
+float aastep (float threshold, float dist) {
+  float afwidth = fwidth(dist) * 0.5;
+  return smoothstep(threshold - afwidth, threshold + afwidth, dist);
+}
+
+void main() {
+    vec2 inp = vec2(vUv.x,vUv.y + uTime * 0.1 ) * 5.0;
+
+    float scale = 16.0;
+    vec2 coord = gl_FragCoord.xy;
+
+    
+    vec2 p = vUv * scaleUp;
+  
+  
+    
+    vec2 f = fract(p);
+    vec3 bgColor = vec3(.0,.0,.1);
+
+    float size = .01;
+    float mixval = 1.0;
+    vec3 wireColor = vec3(0.0,1.0,1.0);
+
+    mixval *= aastep(size, f.x);
+    mixval *= aastep(size, f.y);
+    mixval *= aastep(size, abs(f.x-f.y));
+
+    vec3 fc = mix(wireColor, bgColor, mixval);
+
+
+    // add red line middle
+    vec3 lc = vec3(1.0,0.2,0.2);
+    
+    float distort = (0.05+noise * .100);
+    float lp = .5 + sin(vUv.y * 4. + uTime) * 0.1 * distort;
+    
+    float lw = 0.005;
+    
+
+  // mix the line color
+    lc = mix(vec3(0.0,.5,0.3),lc, noise * .2);
+
+
+    // generate the line
+    // start painting
+    float mixlc  = step(lp, vUv.x) ;
+    // and remove painting
+    mixlc -= step(lp +lw, vUv.x);
+
+    float a2 = texture2D(alphaMap, vUv).r;
+
+    
+    fc = mix(fc, lc, mixlc);
+    gl_FragColor = vec4(fc,a2);
+    
+}
+`,C=`
+varying vec2 vUv;
+varying float noise;
+
+uniform float uTime;
+// uniform vec2 iResolution;
+// uniform vec2 iMouse;
+// uniform float audio1;
+// uniform sampler2D iChannel0;
+// uniform sampler2D iChannel1;
+ 
+//
+// GLSL textureless classic 3D noise "cnoise",
+// with an RSL-style periodic variant "pnoise".
+// Author:  Stefan Gustavson (stefan.gustavson@liu.se)
+// Version: 2011-10-11
+//
+// Many thanks to Ian McEwan of Ashima Arts for the
+// ideas for permutation and gradient selection.
+//
+// Copyright (c) 2011 Stefan Gustavson. All rights reserved.
+// Distributed under the MIT license. See LICENSE file.
+// https://github.com/stegu/webgl-noise
+//
+
+vec3 mod289(vec3 x)
+{
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 mod289(vec4 x)
+{
+  return x - floor(x * (1.0 / 289.0)) * 289.0;
+}
+
+vec4 permute(vec4 x)
+{
+  return mod289(((x*34.0)+10.0)*x);
+}
+
+vec4 taylorInvSqrt(vec4 r)
+{
+  return 1.79284291400159 - 0.85373472095314 * r;
+}
+
+vec3 fade(vec3 t) {
+  return t*t*t*(t*(t*6.0-15.0)+10.0);
+}
+
+// Classic Perlin noise
+float cnoise(vec3 P)
+{
+  vec3 Pi0 = floor(P); // Integer part for indexing
+  vec3 Pi1 = Pi0 + vec3(1.0); // Integer part + 1
+  Pi0 = mod289(Pi0);
+  Pi1 = mod289(Pi1);
+  vec3 Pf0 = fract(P); // Fractional part for interpolation
+  vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+  vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+  vec4 iy = vec4(Pi0.yy, Pi1.yy);
+  vec4 iz0 = Pi0.zzzz;
+  vec4 iz1 = Pi1.zzzz;
+
+  vec4 ixy = permute(permute(ix) + iy);
+  vec4 ixy0 = permute(ixy + iz0);
+  vec4 ixy1 = permute(ixy + iz1);
+
+  vec4 gx0 = ixy0 * (1.0 / 7.0);
+  vec4 gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;
+  gx0 = fract(gx0);
+  vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+  vec4 sz0 = step(gz0, vec4(0.0));
+  gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+  gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+
+  vec4 gx1 = ixy1 * (1.0 / 7.0);
+  vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;
+  gx1 = fract(gx1);
+  vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+  vec4 sz1 = step(gz1, vec4(0.0));
+  gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+  gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+
+  vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+  vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+  vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+  vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+  vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+  vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+  vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+  vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+
+  vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+  g000 *= norm0.x;
+  g010 *= norm0.y;
+  g100 *= norm0.z;
+  g110 *= norm0.w;
+  vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+  g001 *= norm1.x;
+  g011 *= norm1.y;
+  g101 *= norm1.z;
+  g111 *= norm1.w;
+
+  float n000 = dot(g000, Pf0);
+  float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+  float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+  float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+  float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+  float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+  float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+  float n111 = dot(g111, Pf1);
+
+  vec3 fade_xyz = fade(Pf0);
+  vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+  vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+  float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x); 
+  return 2.2 * n_xyz;
+}
+
+// Classic Perlin noise, periodic variant
+float pnoise(vec3 P, vec3 rep)
+{
+  vec3 Pi0 = mod(floor(P), rep); // Integer part, modulo period
+  vec3 Pi1 = mod(Pi0 + vec3(1.0), rep); // Integer part + 1, mod period
+  Pi0 = mod289(Pi0);
+  Pi1 = mod289(Pi1);
+  vec3 Pf0 = fract(P); // Fractional part for interpolation
+  vec3 Pf1 = Pf0 - vec3(1.0); // Fractional part - 1.0
+  vec4 ix = vec4(Pi0.x, Pi1.x, Pi0.x, Pi1.x);
+  vec4 iy = vec4(Pi0.yy, Pi1.yy);
+  vec4 iz0 = Pi0.zzzz;
+  vec4 iz1 = Pi1.zzzz;
+
+  vec4 ixy = permute(permute(ix) + iy);
+  vec4 ixy0 = permute(ixy + iz0);
+  vec4 ixy1 = permute(ixy + iz1);
+
+  vec4 gx0 = ixy0 * (1.0 / 7.0);
+  vec4 gy0 = fract(floor(gx0) * (1.0 / 7.0)) - 0.5;
+  gx0 = fract(gx0);
+  vec4 gz0 = vec4(0.5) - abs(gx0) - abs(gy0);
+  vec4 sz0 = step(gz0, vec4(0.0));
+  gx0 -= sz0 * (step(0.0, gx0) - 0.5);
+  gy0 -= sz0 * (step(0.0, gy0) - 0.5);
+
+  vec4 gx1 = ixy1 * (1.0 / 7.0);
+  vec4 gy1 = fract(floor(gx1) * (1.0 / 7.0)) - 0.5;
+  gx1 = fract(gx1);
+  vec4 gz1 = vec4(0.5) - abs(gx1) - abs(gy1);
+  vec4 sz1 = step(gz1, vec4(0.0));
+  gx1 -= sz1 * (step(0.0, gx1) - 0.5);
+  gy1 -= sz1 * (step(0.0, gy1) - 0.5);
+
+  vec3 g000 = vec3(gx0.x,gy0.x,gz0.x);
+  vec3 g100 = vec3(gx0.y,gy0.y,gz0.y);
+  vec3 g010 = vec3(gx0.z,gy0.z,gz0.z);
+  vec3 g110 = vec3(gx0.w,gy0.w,gz0.w);
+  vec3 g001 = vec3(gx1.x,gy1.x,gz1.x);
+  vec3 g101 = vec3(gx1.y,gy1.y,gz1.y);
+  vec3 g011 = vec3(gx1.z,gy1.z,gz1.z);
+  vec3 g111 = vec3(gx1.w,gy1.w,gz1.w);
+
+  vec4 norm0 = taylorInvSqrt(vec4(dot(g000, g000), dot(g010, g010), dot(g100, g100), dot(g110, g110)));
+  g000 *= norm0.x;
+  g010 *= norm0.y;
+  g100 *= norm0.z;
+  g110 *= norm0.w;
+  vec4 norm1 = taylorInvSqrt(vec4(dot(g001, g001), dot(g011, g011), dot(g101, g101), dot(g111, g111)));
+  g001 *= norm1.x;
+  g011 *= norm1.y;
+  g101 *= norm1.z;
+  g111 *= norm1.w;
+
+  float n000 = dot(g000, Pf0);
+  float n100 = dot(g100, vec3(Pf1.x, Pf0.yz));
+  float n010 = dot(g010, vec3(Pf0.x, Pf1.y, Pf0.z));
+  float n110 = dot(g110, vec3(Pf1.xy, Pf0.z));
+  float n001 = dot(g001, vec3(Pf0.xy, Pf1.z));
+  float n101 = dot(g101, vec3(Pf1.x, Pf0.y, Pf1.z));
+  float n011 = dot(g011, vec3(Pf0.x, Pf1.yz));
+  float n111 = dot(g111, Pf1);
+
+  vec3 fade_xyz = fade(Pf0);
+  vec4 n_z = mix(vec4(n000, n100, n010, n110), vec4(n001, n101, n011, n111), fade_xyz.z);
+  vec2 n_yz = mix(n_z.xy, n_z.zw, fade_xyz.y);
+  float n_xyz = mix(n_yz.x, n_yz.y, fade_xyz.x); 
+  return 2.2 * n_xyz;
+}
+
+float turbulence( vec3 p ) {
+
+  float w = 100.0;
+  float t = -.5;
+
+  for (float f = 1.0 ; f <= 10.0 ; f++ ){
+    float power = pow( 2.0, f );
+    t += abs( pnoise( vec3( power * p ), vec3( 10.0, 10.0, 10.0 ) ) / power );
+  }
+
+  return t;
+
+}
+
+void main() {
+  vUv = uv;
+
+  float offset = uTime * .2;
+
+  // get a turbulent 3d noise using the normal, normal to high freq
+  //noise = 10.0 *  -.50 * turbulence( .5 * normal );
+  // get a 3d noise using the position, low frequency
+  float b = 25.0 * pnoise( 4.0 * vec3(uv.x,uv.y + offset, 1.0), vec3( 100.0 ) );
+  noise = b;
+  // compose both noises
+  //float displacement = - 10. * noise;
+
+  vec4 mvPosition = modelViewMatrix * vec4(position, 2.0 );
+
+    //gl_PointSize = 2. +(500. * uTime ) * (1. / -mvPosition.z);
+    //vec3 newPos = position  * displacement;
+    //newPos.z = displacement;
+  //gl_Position = projectionMatrix * modelViewMatrix * vec4(newPos, 1.0);
+  
+  vec2 suv = vUv;
+  mvPosition.y -= b * 1.1;
+  
+  gl_Position = projectionMatrix * mvPosition;
+  //gl_Position.z = sin(uv.y);
+  //gl_Position.x += sin(uTime) * 0.1;
+}`,n=new x;n.load(b);n.load(S);const v=n.load(U);new d;const T=document.querySelector("canvas"),a=new y;let g=16*12;const r=64,I=new m(g,g,r,r),c=new z({uniforms:{uTime:{value:0},uResolution:{type:"v2",value:new p},alphaMap:{type:"t",value:v},scaleUp:{value:r}},transparent:!0,depthTest:!1,fragmentShader:M,vertexShader:C,alphaMap:v});c.uniforms.uResolution.value.x=window.innerWidth;c.uniforms.uResolution.value.y=window.innerHeight;const i=new u(I,c);i.rotation.x=181;i.position.z=2;a.add(i);const e={width:window.innerWidth,height:window.innerHeight};window.addEventListener("resize",()=>{e.width=window.innerWidth,e.height=window.innerHeight,t.aspect=e.width/e.height,t.updateProjectionMatrix(),o.setSize(e.width,e.height),o.setPixelRatio(Math.min(window.devicePixelRatio,2))});const t=new P(75,e.width/e.height,1,3e3);t.position.x=0;t.position.y=1;t.position.z=25;a.add(t);const o=new w({canvas:T,antialias:!0});o.setSize(e.width,e.height);o.setPixelRatio(Math.min(window.devicePixelRatio,2));const s=new h(t,o.domElement);s.enableDamping=!0;const R=new _,l=()=>{const f=R.getElapsedTime();i.material.uniforms.uTime.value=f,s.update(),o.render(a,t),window.requestAnimationFrame(l)};l();
+//# sourceMappingURL=bb2.9d95b556.js.map
---
+++ assets/headlights.ec3fb5b3.js
@@ -0,0 +1,117 @@
+var W=Object.defineProperty,k=Object.defineProperties;var I=Object.getOwnPropertyDescriptors;var z=Object.getOwnPropertySymbols;var R=Object.prototype.hasOwnProperty,O=Object.prototype.propertyIsEnumerable;var D=(i,e,t)=>e in i?W(i,e,{enumerable:!0,configurable:!0,writable:!0,value:t}):i[e]=t,a=(i,e)=>{for(var t in e||(e={}))R.call(e,t)&&D(i,t,e[t]);if(z)for(var t of z(e))O.call(e,t)&&D(i,t,e[t]);return i},d=(i,e)=>k(i,I(e));import"./modulepreload-polyfill.b7f2da20.js";import{W as U,P as X,S as j,i as E,z as V,m as M,H as B,I as G,c,J as p,b as P,K as Y,Q as q,X as H,Y as r,g as T,f as K,n as L}from"./vendor.23931e5f.js";class J{constructor(e){this.container=e,this.renderer=new U({antialias:!0}),this.renderer.setSize(e.offsetWidth,e.offsetHeight,!1),this.renderer.setPixelRatio(window.devicePixelRatio),e.append(this.renderer.domElement),this.camera=new X(45,e.offsetWidth/e.offsetHeight,.1,1e4),this.scene=new j,this.clock=new E,this.assets={},this.disposed=!1,this.tick=this.tick.bind(this),this.init=this.init.bind(this)}loadAssets(){return new Promise((e,t)=>{})}init(){this.tick()}getViewSizeAtDepth(e=0){const t=this.camera.fov*Math.PI/180,o=Math.abs((this.camera.position.z-e)*Math.tan(t/2)*2);return{width:o*this.camera.aspect,height:o}}dispose(){this.disposed=!0}onResize(){}update(){}render(){this.renderer.render(this.scene,this.camera)}tick(){if(this.disposed)return;if(Q(this.renderer)){const t=this.renderer.domElement;this.camera.aspect=t.clientWidth/t.clientHeight,this.camera.updateProjectionMatrix(),this.onResize()}const e=this.clock.getDelta();this.render(e),this.update(e),requestAnimationFrame(this.tick)}}function Q(i){const e=i.domElement,t=e.clientWidth,o=e.clientHeight,s=e.width!==t||e.height!==o;return s&&i.setSize(t,o,!1),s}const Z=i=>Array.isArray(i)?i[Math.floor(Math.random()*i.length)]:i;class A{constructor(e,t){this.app=e,this.options=t,this.fragmentShader=`
+    #include <fog_pars_fragment>
+    uniform vec3 uColor;
+      varying vec2 vUv;
+      uniform float uSpeed;
+      varying float vprogress;
+      uniform float uTime;
+      varying vec3 vColors;
+
+
+      void main(){
+          
+          float d = step(0.0, uSpeed);
+          float alpha = smoothstep(1.0 - d, d + .0, vUv.x);
+
+        //   vec3 color1 = vec3(0.984,0.773,0.043);
+        //   vec3 color2 = vec3(0.871,0.365,0.);
+        //   float mixValue = distance(vUv.x,vec2(0,1));
+        //   vec3 color = mix(color1,color2,mixValue);
+
+
+            vec3 color = vColors;
+        //color *= 1.5 + sin(vprogress * 90. + uTime );
+
+          alpha *= smoothstep(0.,.2, 1.-vprogress);
+          gl_FragColor = vec4(color,alpha);
+          if (gl_FragColor.a < 0.0001) discard;
+          
+
+      }
+  `,this.vertexShader=`
+    #include <fog_pars_vertex>
+
+    uniform float uTime;
+
+    #include <getDistortion_vertex>
+    uniform float uSpeed;
+    uniform float uTravelLength;
+    varying vec2 vUv;
+    varying float vprogress;
+    varying vec3 vColors;
+    attribute vec3 aOffset;
+    attribute vec3 aColors;
+    attribute vec2 aMetrics;
+      void main() {
+        #include <begin_vertex>
+        #include <project_vertex>
+        #include <fog_vertex>
+        vUv = uv;
+        
+        float radius = aMetrics.r;
+            float len = aMetrics.g;
+            vColors = aColors;
+
+          
+
+        // set the radius and length
+        transformed.xy *= radius;
+        transformed.z *= len;
+
+        // Keep them separated to make the next step easier!
+        float zOffset = uTime * uSpeed + aOffset.z;
+        zOffset = len - mod(zOffset, uTravelLength);
+        transformed.z += zOffset;
+        transformed.xy += aOffset.xy;
+
+        float progress = abs(transformed.z / uTravelLength);
+        vprogress = progress;
+        transformed.xyz += getDistortion(progress);
+        
+    
+        mvPosition = modelViewMatrix * vec4(transformed,1.);
+        gl_Position = projectionMatrix * mvPosition;
+        //gl_Position = projectionMatrix * modelViewMatrix * vec4(position.xyz, 1.);
+
+
+      }
+  `}init(){const e=this.options;let t=new V(new M(0,0,0),new M(0,0,-1)),o=new B(t,20,2,8,!1),s=new G().copy(o);s.instanceCount=e.nPairs*2;let n=this.options.roadWidth/this.options.roadSections,h=[],v=[],f=[];for(let l=0;l<e.nPairs;l++){let u=Math.random()*.1+.1,x=Math.random()*e.length*.08+e.length*.02;const _=Z(e.colors);let w=new c(_).toArray();f.push(...w),f.push(...w),v.push(u,x,u,x);let y=l%3*n-e.roadWidth/2+n/2,S=.5*n,C=.5*Math.random(),F=u*1.3,b=Math.random()*this.options.length;h.push(y-S/2+C),h.push(F),h.push(-b),h.push(y+S/2+C),h.push(F),h.push(-b)}s.setAttribute("aOffset",new p(new Float32Array(h),3,!1)),s.setAttribute("aMetrics",new p(new Float32Array(v),2,!1)),s.setAttribute("aColors",new p(new Float32Array(f),3,!1));let g=new P({fog:!0,fragmentShader:this.fragmentShader,vertexShader:this.vertexShader,transparent:!0,side:Y,uniforms:a(d(a({},q.merge([H.fog])),{uTime:new r(0),uSpeed:new r(e.speed||1),uColor:new r(new c(e.color)),uTravelLength:new r(e.length)}),this.options.distortion.uniforms)});g.onBeforeCompile=l=>{l.vertexShader=l.vertexShader.replace("#include <getDistortion_vertex>",this.options.distortion.vshader)};let m=new T(s,g);m.frustumCulled=!1,this.mesh=m,this.app.scene.add(m)}update(e){this.mesh.material.uniforms.uTime.value=e}}class N{constructor(e,t){this.app=e,this.options=a({},t),this.fragmentShader=`
+      uniform vec3 uColor;
+      void main(){
+          gl_FragColor = vec4(uColor,1.);
+      }
+  `,this.vertexShader=`
+      uniform float uTime;
+
+      #include <getDistortion_vertex>
+      uniform float uTravelLength;
+      void main(){
+          vec3 transformed = position.xyz;
+          float progress = (transformed.y + uTravelLength / 2.) / uTravelLength;
+          vec3 distortion  = getDistortion(progress);
+          transformed.x += distortion.x;
+            transformed.z += distortion.y;
+          gl_Position = projectionMatrix * modelViewMatrix * vec4(transformed.xyz, 1.);
+      }
+  `}init(){const e=this.options,t=new K(e.width,e.length,20,200);let o=new P({fragmentShader:this.fragmentShader,vertexShader:this.vertexShader,uniforms:a({uTime:new r(0),uColor:new r(new c("#000000")),uTravelLength:new r(e.length)},this.options.distortion.uniforms)});o.onBeforeCompile=n=>{n.vertexShader=n.vertexShader.replace("#include <getDistortion_vertex>",this.options.distortion.vshader)};const s=new T(t,o);this.mesh=s,s.rotation.x=-Math.PI/2,s.position.z=-e.length/2,this.app.scene.add(s)}update(e){this.mesh.material.uniforms.uTime.value=e}}const $={uDistortionX:new r(new L(80,3)),uDistortionY:new r(new L(-40,2.5))},ee=`
+  #define PI 3.14159265358979
+    uniform vec2 uDistortionX;
+    uniform vec2 uDistortionY;
+  
+      float nsin(float val){
+      return sin(val) * 0.5+0.5;
+      }
+    vec3 getDistortion(float progress){
+          progress = clamp(progress, 0.,1.);
+          float xAmp = uDistortionX.r;
+          float xFreq = uDistortionX.g;
+          float yAmp = uDistortionY.r;
+          float yFreq = uDistortionY.g;
+          return vec3( 
+              xAmp * nsin(progress* PI * xFreq   - PI / 2.) ,
+              yAmp * nsin(progress * PI *yFreq - PI / 2.  ) ,
+              0.
+          );
+      }
+  `,te={uniforms:$,vshader:ee};class ie extends J{constructor(e){const t=a({container:document.getElementById("container"),length:400,width:20,roadWidth:9,islandWidth:2,nPairs:100,roadSections:3,color:"#111111",distortion:te},e);super(t.container);this.options=t,this.camera.position.z=6,this.camera.position.y=7,this.camera.position.x=0;let o=["#ff0000","#00ff00","#0000ff"],s=["#ff0000","#00ff00","#0000ff"];o=["#fbc50b","#de5d00","#f9f5d6"],s=["#eb6333","#b1d5de","#182c37"],o=["#0CECDD","#FFF338","#FF67E7","#C400FF"],s=["#0E185F","#2FA4FF","#00FFDD","#E8FFC2"],this.road=new N(this,t),this.leftLights=new A(this,d(a({},t),{colors:o,color:65322,speed:30})),this.rightLights=new A(this,d(a({},t),{colors:s,color:16722474,speed:-30}));const n=new c("black");this.scene.background=n}init(){this.road.init(),this.leftLights.init(),this.leftLights.mesh.position.setX(this.options.roadWidth/2+this.options.islandWidth/2),this.rightLights.init(),this.rightLights.mesh.position.setX(-this.options.roadWidth/2-this.options.islandWidth/2),this.tick()}update(e){let t=this.clock.elapsedTime;this.road.update(t),this.leftLights.update(t),this.rightLights.update(t)}}const se=new ie;se.init();
+//# sourceMappingURL=headlights.ec3fb5b3.js.map
